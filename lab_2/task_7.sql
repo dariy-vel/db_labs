df37956821da429ccef368e8a7bf8978c2daf9e4
dariy-vel <dariy.velychko@gmail.com>
2019-10-27 08:55:45 +0200

    Improved selects for lab_2

--- lab_2/task_7.sql
+++ lab_2/task_7.sql
@@ -1,5 +1,7 @@
 USE Labor_SQL;
 SELECT maker FROM Product
-WHERE model IN (SELECT model FROM PC)
+WHERE maker NOT IN 
+	(SELECT maker FROM Product WHERE model NOT IN (SELECT model FROM PC) AND type = 'PC')
+    AND type = 'PC'
 GROUP BY maker
 ;
--- lab_2/task_9.sql
+++ lab_2/task_9.sql
@@ -1,9 +1,9 @@
 USE Labor_SQL;
-SELECT Income_o.point, Income_o.date, inc, Outcome_o.out
+SELECT Income_o.point, Income_o.date, ifnull(inc, 0) as inc,  ifnull(Outcome_o.out, 0) as `out`
 FROM Income_o
 LEFT OUTER JOIN Outcome_o ON Income_o.point = Outcome_o.point AND Income_o.date = Outcome_o.date
 UNION
-SELECT Outcome_o.point, Outcome_o.date, Income_o.inc, Outcome_o.out
+SELECT Outcome_o.point, Outcome_o.date, ifnull(Income_o.inc, 0) as inc, ifnull(Outcome_o.out,0) as `out`
 FROM Outcome_o
 LEFT OUTER JOIN Income_o ON Outcome_o.point = Income_o.point AND Income_o.date = Outcome_o.date
 ORDER BY point, date
